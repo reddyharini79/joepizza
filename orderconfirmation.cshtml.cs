d62cf47f022dba669278029986aa0dceb1e43735
reddyharini79 <147048808+reddyharini79@users.noreply.github.com>
2023-12-14 23:59:19 -0800

    Add files via upload

---
+++ orderconfirmation.cshtml.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class OrderConfirmationModel : PageModel
+{
+    public int OrderId { get; set; }
+
+    public double Amount { get; set; }
+
+    public string SelectedPizza { get; set; }
+
+    public void OnGet(int orderId, double amount, string pizza)
+    {
+        OrderId = orderId;
+        Amount = amount;
+        SelectedPizza = pizza;
+    }
+}
